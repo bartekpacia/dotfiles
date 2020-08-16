ed16aea46b21cd49144e17d0d5144a1710bf8c2b
Bartek Pacia <barpac02@gmail.com>
2020-08-16 11:53:48 +0200

    idk

--- custom/bartekpacia.zsh
+++ custom/bartekpacia.zsh
@@ -1,14 +1,3 @@
-# You can put files here to add functionality separated per file, which
-# will be ignored by git.
-# Files on the custom/ directory will be automatically loaded by the init
-# script, in alphabetical order.
-
-# For example: add yourself some shortcuts to projects you often work on.
-#
-# brainstormr=~/Projects/development/planetargon/brainstormr
-# cd $brainstormr
-#
-
 # Projects
 alias rudy_android='cd ~/dev/android/discover-rudy-android'
 alias rudy_functions='cd ~/dev/node/discover-rudy-functions'
