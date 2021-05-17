56d18118ff78b523e74da645fe66b4564bd68017
Bartek Pacia <barpac02@gmail.com>
2021-05-17 14:52:49 +0200

    add dev-xxx aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -2,37 +2,50 @@
 alias cls="clear"
 
 if command -v lsd &>/dev/null; then
-  alias lsd="lsd -lah"
+    alias lsd="lsd -lah"
 else
-  alias lsd="ls -lah"
+    alias lsd="ls -lah"
 fi
 
 if command -v bat &>/dev/null; then
-  alias cat="bat -p"
+    alias cat="bat -p"
 fi
+
+# ------ Dev ------
+godev() {
+    cd ~/dev/go
+}
+
+nodev() {
+    cd ~/dev/node
+}
+
+pydev() {
+    cd ~/dev/python
+}
 
 # ------ Sleep ------
 
 if command -v pmset &>/dev/null; then
-  alias zzz="pmset sleepnow"
-  alias zzzz="pmset sleepnow"
-  elif command -v systemctl &>/dev/null; then
-  alias zzz="systemctl sleep"
-  alias zzzz="systemctl sleep"
+    alias zzz="pmset sleepnow"
+    alias zzzz="pmset sleepnow"
+    elif command -v systemctl &>/dev/null; then
+    alias zzz="systemctl sleep"
+    alias zzzz="systemctl sleep"
 fi
 
 # ------ IntelliJ IDEs ------
 
 if [ -f ~/dotfiles/idea/idea ]; then
-  alias idea="~/dotfiles/idea/idea"
+    alias idea="~/dotfiles/idea/idea"
 fi
 
 if [ -f ~/dotfiles/idea/pycharm ]; then
-  alias pycharm="~/dotfiles/idea/pycharm"
+    alias pycharm="~/dotfiles/idea/pycharm"
 fi
 
 if [ -f ~/dotfiles/idea/clion ]; then
-  alias clion="~/dotfiles/idea/clion"
+    alias clion="~/dotfiles/idea/clion"
 fi
 
 # ------ C/C++ ------
@@ -42,13 +55,13 @@ alias buildc='gcc -std=c18 -o'
 # ------ Shit ------
 
 lenny() {
-  if [ "$1" = "sad" ]; then
-    printf "( ͡° ʖ̯ ͡°)\n"
-    elif [ "$1" = "sly" ]; then
-    printf "( ͡° ͜ʖ ͡°)\n"
-    elif [ "$1" = "idk" ]; then
-    printf "¯\_(ツ)_/¯\n"
-  else
-    printf "error: name the lenny!\n"
-  fi
+    if [ "$1" = "sad" ]; then
+        printf "( ͡° ʖ̯ ͡°)\n"
+        elif [ "$1" = "sly" ]; then
+        printf "( ͡° ͜ʖ ͡°)\n"
+        elif [ "$1" = "idk" ]; then
+        printf "¯\_(ツ)_/¯\n"
+    else
+        printf "error: name the lenny!\n"
+    fi
 }
