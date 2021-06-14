bcdb42fc26cc9e2e243a20baa3dc9608da331d92
Bartek Pacia <barpac02@gmail.com>
2021-06-14 10:39:20 +0200

    update example

--- local/example.zsh
+++ local/example.zsh
@@ -1,5 +1,11 @@
-# Example local file which will be automatically sourced
-# during initialization.
+# This directory is for machine-specific files that need to be sources
+# during shell startup by ~/.zshrc.
 
+# All files in this directory will be automatically sourced by ~/.zshrc.
+
+# Also, all files in this dir (except this one, of course) are ignored
+# by git.
+
+# This very file is here only to demostrate the purpose of this directory
 # Uncomment to see that it works
 # echo "Hello from a local file"
