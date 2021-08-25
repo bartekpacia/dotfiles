6a4a2f536ecff0c91771784fc65f0e0435b2a81d
Bartek Pacia <barpac02@gmail.com>
2021-08-25 22:14:53 +0200

    update git plugin

--- custom/plugins/git/git.plugin.zsh
+++ custom/plugins/git/git.plugin.zsh
@@ -2,27 +2,20 @@
 # Functions
 #
 
-# The name of the current branch
-# Back-compatibility wrapper for when this function was defined here in
-# the plugin, before being pulled in to core lib/git.zsh as git_current_branch()
-# to fix the core -> git plugin dependency.
-function current_branch() {
-  git_current_branch
-}
 
 # Pretty log messages
 function _git_log_prettily(){
-  if ! [ -z $1 ]; then
-    git log --pretty=$1
-  fi
+    if ! [ -z $1 ]; then
+        git log --pretty=$1
+    fi
 }
 compdef _git _git_log_prettily=git-log
 
 # Warn if the current branch is a WIP
 function work_in_progress() {
-  if $(git log -n 1 2>/dev/null | grep -q -c "\-\-wip\-\-"); then
-    echo "WIP!!"
-  fi
+    if $(git log -n 1 2>/dev/null | grep -q -c "\-\-wip\-\-"); then
+        echo "WIP!!"
+    fi
 }
 
 #
@@ -102,48 +95,48 @@ alias gg='git gui citool'
 alias gga='git gui citool --amend'
 
 function ggf() {
-  [[ "$#" != 1 ]] && local b="$(git_current_branch)"
-  git push --force origin "${b:=$1}"
+    [[ "$#" != 1 ]] && local b="$(git_current_branch)"
+    git push --force origin "${b:=$1}"
 }
 compdef _git ggf=git-checkout
 function ggfl() {
-  [[ "$#" != 1 ]] && local b="$(git_current_branch)"
-  git push --force-with-lease origin "${b:=$1}"
+    [[ "$#" != 1 ]] && local b="$(git_current_branch)"
+    git push --force-with-lease origin "${b:=$1}"
 }
 compdef _git ggfl=git-checkout
 
 function ggl() {
-  if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
-    git pull origin "${*}"
-  else
-    [[ "$#" == 0 ]] && local b="$(git_current_branch)"
-    git pull origin "${b:=$1}"
-  fi
+    if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
+        git pull origin "${*}"
+    else
+        [[ "$#" == 0 ]] && local b="$(git_current_branch)"
+        git pull origin "${b:=$1}"
+    fi
 }
 compdef _git ggl=git-checkout
 
 function ggp() {
-  if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
-    git push origin "${*}"
-  else
-    [[ "$#" == 0 ]] && local b="$(git_current_branch)"
-    git push origin "${b:=$1}"
-  fi
+    if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
+        git push origin "${*}"
+    else
+        [[ "$#" == 0 ]] && local b="$(git_current_branch)"
+        git push origin "${b:=$1}"
+    fi
 }
 compdef _git ggp=git-checkout
 
 function ggpnp() {
-  if [[ "$#" == 0 ]]; then
-    ggl && ggp
-  else
-    ggl "${*}" && ggp "${*}"
-  fi
+    if [[ "$#" == 0 ]]; then
+        ggl && ggp
+    else
+        ggl "${*}" && ggp "${*}"
+    fi
 }
 compdef _git ggpnp=git-checkout
 
 function ggu() {
-  [[ "$#" != 1 ]] && local b="$(git_current_branch)"
-  git pull --rebase origin "${b:=$1}"
+    [[ "$#" != 1 ]] && local b="$(git_current_branch)"
+    git pull --rebase origin "${b:=$1}"
 }
 compdef _git ggu=git-checkout
 
@@ -158,10 +151,6 @@ alias ghh='git help'
 
 alias gignore='git update-index --assume-unchanged'
 alias gignored='git ls-files -v | grep "^[[:lower:]]"'
-alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
-
-alias gk='\gitk --all --branches'
-alias gke='\gitk --all $(git log -g --pretty=%h)'
 
 alias gl='git log'
 alias glo='git log --graph --oneline'
@@ -261,15 +250,15 @@ alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "--wip-- [skip ci]"'
 
 function grename() {
-  if [[ -z "$1" || -z "$2" ]]; then
-    echo "Usage: $0 old_branch new_branch"
-    return 1
-  fi
-  
-  # Rename branch locally
-  git branch -m "$1" "$2"
-  # Rename branch in origin remote
-  if git push origin :"$1"; then
-    git push --set-upstream origin "$2"
-  fi
+    if [[ -z "$1" || -z "$2" ]]; then
+        echo "Usage: $0 old_branch new_branch"
+        return 1
+    fi
+    
+    # Rename branch locally
+    git branch -m "$1" "$2"
+    # Rename branch in origin remote
+    if git push origin :"$1"; then
+        git push --set-upstream origin "$2"
+    fi
 }
